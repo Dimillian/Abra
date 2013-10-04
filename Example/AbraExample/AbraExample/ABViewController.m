99ecb3fbae2b994ff6ae3df31ac187666ba2000c
Thomas Ricouard <ricouard77@gmail.com>
2013-10-04 16:06:36 +0200

    Add example

---
+++ Example/AbraExample/AbraExample/ABViewController.m
@@ -0,0 +1,29 @@
+//
+//  ABViewController.m
+//  AbraExample
+//
+//  Created by Thomas Ricouard on 04/10/13.
+//  Copyright (c) 2013 Thomas Ricouard. All rights reserved.
+//
+
+#import "ABViewController.h"
+
+@interface ABViewController ()
+
+@end
+
+@implementation ABViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/AbraExample/AbraExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AbraExample
+//
+//  Created by Thomas Ricouard on 04/10/13.
+//  Copyright (c) 2013 Thomas Ricouard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABAppDelegate class]));
+    }
+}
